b78819d985b235d2eaa239c1670e0a93c237ad95
Nathan Tarbert <nate4t@hotmail.comm>
2021-01-30 19:12:46 -0800

    added partials, details page etc

--- routes/about.js
+++ routes/about.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const Cube = require('../models/cube');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Cube.find().then((cube) => {
-    res.render('index', {title:"Cubicle", cube: cube});
-  });
+  res.render('about', { title: 'About' });
 });
+
 module.exports = router;
--- routes/create.js
+++ routes/create.js
@@ -4,31 +4,61 @@ const cubes = require('../config/database.json');
 const cubesPath = './config/database.json';
 const fs = require('fs');
 const Cube = require("../models/cube");
+const Accessory = require("../models/accessory");
 const {handlebars} = require('hbs');
 /* GET add cube page*/
 router.get('/', function(req, res, next) {
     res.render('create', { title: 'Create a Cube' });
 });
+
+router.get('/accessory', function(req, res, next) {
+    res.render('accessory', { title: 'Create Accessory'});
+});
 //make your form post request script
 router.post('/', function(req, res, next) {
-    console.log('incoming form submission', req.body);
+    console.log('incoming', req.body);
+
     const newCube = new Cube({
         name: req.body.name,
         description: req.body.description,
-        image_url: req.body.image_url,
-        level: req.body.difficultyLevel,
+        imageUrl: req.body.imageUrl,
+        difficulty: req.body.difficultyLevel,
+        accessories: []
     });
-    
+
     //this is a promise...
     newCube.save()
     .then((result) => {
         console.log(result);
         res.redirect('/');
-        })
+    })
         .catch((err) => {
+        res.send(err);
+    });
+
+    router.post('/accessory', function(req, res, next) {
+        console.log('incoming form submission', req.body);
+    
+        const newCube = new Cube({
+            name: req.body.name,
+            description: req.body.description,
+            imageUrl: req.body.imageUrl,
+            difficulty: req.body.difficultyLevel,
+            
+        });
+    
+        //this is a promise...
+        newCube.save()
+        .then((result) => {
+            console.log(result);
+            res.redirect('/');
+        })
+            .catch((err) => {
             res.send(err);
         });
+    });
 });
+
 module.exports = router;
 
 

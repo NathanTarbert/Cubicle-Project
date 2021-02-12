919ba7bae1439dbeb8df2cb27a84bbbf360a42c0
Nathan Tarbert <nate4t@hotmail.comm>
2021-02-12 14:08:08 -0800

    edit working

--- routes/edit.js
+++ routes/edit.js
@@ -6,13 +6,28 @@ const Cube = require("../models/cube");
 router.get('/:id/', (req, res, next) => {
     let id = req.params.id;
     console.log('edit id is', id);
-    Cube.findOne((err, cube) => {
-        if (err) return res.status(500).send(err);
-        res.render('editCubePage', {cube: cube, user: req.user});
-    });
+    Cube.findOne({_id: id})
+        .then((foundCube) => {
+             res.render('editCubePage', {cube: foundCube, user: req.user});
+    });    
 });
-// router.post('/:id', (req, res) => {
-//     let id = req.params.id;
-//     res.redirect('/');
-// });
+
+router.post('/:id', async (req, res) => {    
+    let cube;
+     try {
+        cube = await Cube.findById(req.params.id);
+        cube.name = req.body.name, 
+        cube.description = req.body.description, 
+        cube.imageUrl = req.body.imageUrl, 
+        cube.difficulty = req.body.difficultyLevel,
+        await cube.save();
+        res.redirect(`/`);
+    }catch {
+        if (cube == null) {
+            res.redirect('/');
+        }else {
+             res.render('details', { cube: cube, errorMessage: 'Error Editing Cube'});
+        }       
+    }     
+});
   module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Cube = require('../models/cube');
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
